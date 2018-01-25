c5184eaf698052af10b868bba8183ca3326e8d03
AmarjeetYadav <35659897+AmarjeetYadav@users.noreply.github.com>
2018-01-25 14:31:46 +0530

    greater.py

---
+++ greater.py
@@ -0,0 +1,10 @@
+a=int(input("enter no a:"))
+b=int(input("enter no b:"))
+c=int(input("enter no c:"))
+if(a>b):
+    print("a is greater")
+elif(b>c):
+    print("b is greater")
+else:
+    print("c is greater")
+    
